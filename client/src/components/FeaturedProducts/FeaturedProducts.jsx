324e8f0de940f0f2c0c2a1a1bb7c1989114a8fc8
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-11-29 09:43:41 +0200

    featured/treding products component added

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,62 @@
+import Card from "../Card/Card";
+import "./FeaturedProducts.scss";
+
+const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dCUyMHNoaXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60:",
+      title: "Long Sleeve Graphic T-shirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dCUyMHNoaXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60:",
+      img2: "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      title: "Long Sleeve Graphic T-shirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dCUyMHNoaXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60:",
+      title: "Long Sleeve Graphic T-shirt",
+      isNew: false,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dCUyMHNoaXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60:",
+      img2: "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      title: "Long Sleeve Graphic T-shirt",
+      isNew: false,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam tempora
+          tenetur corrupti quis harum illum praesentium, alias necessitatibus
+          quam totam perferendis accusantium atque laboriosam ipsum nemo
+          facilis, consequatur vitae quod?
+        </p>
+      </div>
+      <div className="bottom">
+        {data.map((item) => (
+          <Card item={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
